d15b5a7c4031b52f77bd13b4f93e7dc055d3c9bc
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-20 14:51:38 +0000

    Pagination added in getBooks API

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -36,8 +36,24 @@ router.post("/", protectRoute, async (req, res) => {
 
 router.get("/", protectRoute, async (req, res) => {
   try {
-    const books = await Book.find();
-    res.send(books);
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 5;
+
+    const skip = (page - 1) * limit;
+    const books = await Book.find()
+      .sort({ createdAt: -1 }) //des
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "userName profileImage");
+
+    const totalBooks = await Book.countDocuments();
+
+    res.send({
+      books,
+      currentPage:page,
+      totalBooks,
+      totalPages:Math.ceil(totalBooks/limit)
+    });
   } catch (error) {
     console.log("Error in fetching books", error);
     res.status(500).json({ message: "Internal Server Error" });
