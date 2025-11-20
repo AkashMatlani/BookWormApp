7b1b2ecaf738a1d6b44dc2381f6939e2728191a7
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-20 13:59:24 +0000

    Cloudinary image save to Database

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@
 import express from "express";
 import "dotenv/config";
 
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js"
+
 import { connectDB } from "./lib/db.js";
  
 const app = express();
@@ -12,8 +14,9 @@ const PORT = process.env.PORT || 3000;
 console.log("Mongo URI:", process.env.MONGO_URI);
 app.use(express.json());
 
-
 app.use("/api/auth",authRoutes);
+app.use("/api/auth",bookRoutes);
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,5 +1,34 @@
 import express from "express";
 
-const router =express.Router();
+import cloudinary from "../lib/cloudinary";
+import Book from "../models/Book.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
+    if (!image || !title || !caption || !rating) {
+      return res.status(400).json({ message: "Please provide all fileds" });
+    }
+
+    //upload the image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+
+    //save to the database
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+    });
+
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
