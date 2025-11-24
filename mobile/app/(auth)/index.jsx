29c1ec1d5e9084b213023a32676a233ea15ccdc4
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-24 15:13:47 +0000

    Email Input style Added

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,22 +1,53 @@
 import { useState } from "react";
-import { View,Image } from "react-native";
+import { View, Image, Text, TextInput } from "react-native";
 import styles from "../../assets/styles/login.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   return (
     <View style={styles.container}>
-        {/* ILLUSTRATION Image */}
-        <View style={styles.topIllustration} >
-            <Image style={styles.illustrainImage}
-             source={require("../../assets/images/i.png")}
-             resizeMode="containr"
-             ></Image>
+      {/* ILLUSTRATION Image */}
+      <View style={styles.topIllustration}>
+        <Image
+          style={styles.illustrainImage}
+          source={require("../../assets/images/i.png")}
+          resizeMode="containr"
+        ></Image>
+      </View>
+
+      {/* Card */}
+      <View style={styles.card}>
+        <View style={styles.formConainer}>
+          {/* EMAIL */}
+          <View style={styles.inputGroup}>
+            <Text style={styles.label}> Email</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons
+                name="mail-outline"
+                size={20}
+                color={COLORS.primary}
+                style={styles.inputIcon}
+              ></Ionicons>
+
+              <TextInput
+                style={styles.input}
+                placeholder="Enter Your Email"
+                placeholderTextColor={COLORS.placeholderText}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              ></TextInput>
+            </View>
+          </View>
         </View>
+      </View>
     </View>
   );
 }
--- mobile/assets/styles/login.styles.js
+++ mobile/assets/styles/login.styles.js
@@ -19,6 +19,49 @@ const styles = StyleSheet.create({
     width: width * 0.75,
     height: width * 0.75,
   },
+  card: {
+    backgroundColor: COLORS.cardBackground,
+    borderRadius: 16,
+    padding: 24,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 4,
+    borderWidth: 2,
+    borderColor: COLORS.border,
+    marginTop: -24,
+  },
+  formConainer: {
+    marginBottom: 16,
+  },
+  inputGroup: {
+    marginBottom: 20,
+  },
+  label: {
+    color: COLORS.textPrimary,
+    fontSize: 14,
+    marginBottom: 8,
+    fontWeight: "500",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    borderWidth: 1,
+    borderRadius: 12,
+    borderColor: COLORS.border,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    backgroundColor: COLORS.inputBackground,
+  },
+
+  inputIcon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    color: COLORS.textDark,
+  },
 });
 
 export default styles;
