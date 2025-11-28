d209cb93071ca79ddff7a93a9c56bc70233beb50
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-28 13:01:41 +0000

    Handle navigation based on auth state

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,16 +1,36 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { useAuthStore } from "../store/authStore";
+import { Slot } from "expo-router";
 
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  //handle navigation based on auth state
+
+  useEffect(() => {
+    if (segments.length === 0) return;
+
+    const isAuthScreen = segments[0] === "(auth)";
+    const isSigned = user && token;
+
+    if (!isSigned && !isAuthScreen) router.replace("/(auth)");
+    else if (isSigned && isAuthScreen) router.replace("/(tabs)");
+  }, [user, token, segments]);
   return (
     <SafeAreaProvider>
       <SafeScreen>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="(tabs)"></Stack.Screen>
-          <Stack.Screen name="(auth)"></Stack.Screen>
-        </Stack>
+        <Slot />
       </SafeScreen>
       <StatusBar style="dark" />
     </SafeAreaProvider>
