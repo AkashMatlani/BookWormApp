6ad22b07e5530d3e7427c60105efb1da32e2a397
Akash Matlani <aakash.matlani@yahoo.com>
2025-11-28 13:18:28 +0000

    Tab bar design Added

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -2,10 +2,27 @@ import { View, Text } from "react-native";
 import { Tabs } from "expo-router";
 
 import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: COLORS.primary,
+        headerTitleStyle: {
+          color: COLORS.textPrimary,
+          fontWeight: "600",
+        },
+        headerShadowVisible: false,
+
+        tabBarStyle: {
+          backgroundColor: COLORS.cardBackground,
+          borderTopWidth: 1,
+          borderTopColor: COLORS.border,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
