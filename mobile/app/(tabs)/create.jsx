e9f01dcf5ec1834602e64eb123fd96509d063f06
Akash Matlani <aakash.matlani@yahoo.com>
2025-12-12 15:24:37 +0000

    Book Title added

--- mobile/app/(tabs)/create.jsx
+++ mobile/app/(tabs)/create.jsx
@@ -4,10 +4,15 @@ import {
   Platform,
   ScrollView,
   Text,
+  TextInput,
 } from "react-native";
 import styles from "../../assets/styles/create.styles";
+import { Ionicons } from "@expo/vector-icons";
+import COLORS from "../../constants/colors";
+import { useState } from "react";
 
 const CreateScreen = () => {
+  const { title, setTitle } = useState("");
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
@@ -24,6 +29,28 @@ const CreateScreen = () => {
             <Text style={styles.subTitle}>
               Share your favorite reads with others
             </Text>
+          </View>
+          {/*  Book Title */}
+          <View style={styles.form}>
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Title</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="book-outline"
+                  size={20}
+                  color={COLORS.textSecondary}
+                  style={styles.inputIcon}
+                ></Ionicons>
+
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter book title"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={title}
+                  onChange={setTitle}
+                ></TextInput>
+              </View>
+            </View>
           </View>
         </View>
       </ScrollView>
--- mobile/assets/styles/create.styles.js
+++ mobile/assets/styles/create.styles.js
@@ -11,34 +11,63 @@ const style = StyleSheet.create({
     flex: 1,
     backgroundColor: COLORS.background,
   },
-  card:{
-    backgroundColor:COLORS.background,
-    borderRadius:16,
-    padding:20,
-    marginVertical:16,
-    shadowColor:COLORS.black,
-    shadowOffset:{width:0,height:2},
-    shadowOpacity:0.1,
-    shadowRadius:8,
-    elevation:3,
-    borderWidth:1,
-    borderColor:COLORS.border
+  card: {
+    backgroundColor: COLORS.background,
+    borderRadius: 16,
+    padding: 20,
+    marginVertical: 16,
+    shadowColor: COLORS.black,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 3,
+    borderWidth: 1,
+    borderColor: COLORS.border,
   },
-  header:{
-    alignItems:"center",
-    marginBottom:24
+  header: {
+    alignItems: "center",
+    marginBottom: 24,
   },
-  title:{
-   fontSize:24,
-   fontWeight:"700",
-   color:COLORS.textPrimary,
-   marginBottom:8, 
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: COLORS.textPrimary,
+    marginBottom: 8,
   },
-  subTitle:{
-    fontSize:14,
-    color:COLORS.textSecondary,
-    textAlign:"center"
-  }
+  subTitle: {
+    fontSize: 14,
+    color: COLORS.textSecondary,
+    textAlign: "center",
+  },
+  form: {
+    marginBottom: 16,
+  },
+  formGroup: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 14,
+    marginBottom: 8,
+    color: COLORS.textPrimary,
+    fontWeight: "500",
+  },
+  inputContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: COLORS.inputBackground,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: COLORS.border,
+    paddingHorizontal: 12,
+  },
+  inputIcon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    height: 48,
+    color: COLORS.textDark,
+  },
 });
 
 export default style;
