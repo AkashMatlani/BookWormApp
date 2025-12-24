6dde9be823a04a3883b21ffed070298f45125d36
Akash Matlani <aakash.matlani@yahoo.com>
2025-12-24 17:12:38 +0000

    Logout Feature added

--- mobile/app/(tabs)/index.jsx
+++ mobile/app/(tabs)/index.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useAuthStore } from '../../store/authStore'
 
 const indexTab = () => {
+
+  const {logout} =useAuthStore();
   return (
     <View>
-      <Text>Tab</Text>
+      <Text>Home Tab</Text>
+      <TouchableOpacity onPress={logout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   )
 }
